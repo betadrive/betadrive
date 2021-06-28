0242ed0057bc1236e95ea0c7d3bcf367fe281e63
Christian Matthy <cjmatthy200@gmail.com>
2021-06-28 15:26:27 -0400

    add recipe for robofist

--- src/main/java/com/matthyfamily/betadrive/item/RobofistItem.java
+++ src/main/java/com/matthyfamily/betadrive/item/RobofistItem.java
@@ -12,40 +12,13 @@ import net.minecraft.util.Hand;
 import java.util.Random;
 
 public class RobofistItem extends SwordItem {
-    public static String getCardinalDirection(PlayerEntity player) {
-        double rotation = (player.getHeadYaw() - 90) % 360;
-        if (rotation < 0) {
-            rotation += 360.0;
-        }
-        if (0 <= rotation && rotation < 22.5) {
-            return "N";
-        } else if (22.5 <= rotation && rotation < 67.5) {
-            return "NE";
-        } else if (67.5 <= rotation && rotation < 112.5) {
-            return "E";
-        } else if (112.5 <= rotation && rotation < 157.5) {
-            return "SE";
-        } else if (157.5 <= rotation && rotation < 202.5) {
-            return "S";
-        } else if (202.5 <= rotation && rotation < 247.5) {
-            return "SW";
-        } else if (247.5 <= rotation && rotation < 292.5) {
-            return "W";
-        } else if (292.5 <= rotation && rotation < 337.5) {
-            return "NW";
-        } else if (337.5 <= rotation && rotation < 360.0) {
-            return "N";
-        } else {
-            return null;
-        }
-    }
     public RobofistItem(ToolMaterial toolMaterial, int attackDamage, float attackSpeed, Settings settings) {
         super(toolMaterial, attackDamage, attackSpeed, settings);
     }
 
     @Override
     public ActionResult useOnEntity(ItemStack stack, PlayerEntity user, LivingEntity entity, Hand hand) {
-        entity.takeKnockback(25, RANDOM.nextFloat()*100, RANDOM.nextFloat()*100);
+        entity.takeKnockback(5, RANDOM.nextFloat()*100, RANDOM.nextFloat()*100);
 
         return super.useOnEntity(stack, user, entity, hand);
     }
