0ae46a918194cb7c7c968fe6abbe8a10d074ec6b
karpejev <glebkarpejev828@gmail.com>
2025-10-21 00:05:48 +0300

    Changed SpeedText to calculate real movement speed instead of hardcoded values

--- src/main/java/dev/matthy/betadrive/hud/texts/SpeedText.java
+++ src/main/java/dev/matthy/betadrive/hud/texts/SpeedText.java
@@ -4,14 +4,23 @@ import dev.matthy.betadrive.hud.HUDText;
 import net.minecraft.util.math.Vec3d;
 
 public class SpeedText extends HUDText {
+    private static Vec3d prevPos = null;
+    private static long prevTick = 0;
+    private static double prevVel = 0;
     public SpeedText() {
-        super("MPS", (player, world) -> { // this function sucks! really bad! Doesn't account for elytra/creative flight/minecart/boat/etc.
-            Vec3d origin = new Vec3d(0,0,0);
-            if((int) (player.getVelocity().distanceTo(origin)*1000) == 78) return "0"; // origin is a Vec3d = (0,0,0), checks if you're not moving
-            int speed = Math.round(1000 * player.getMovementSpeed()); // only shows if running or not, but even if you're not moving it shows as 100
-            if(speed == 100) return "4.317"; // walking
-            if(speed == 130) return "5.612"; // running
-            return "0"; // probably shouldn't happen!
+        super("MPS", (player, world) -> { // this function rocks! really not bad! Does account for elytra/creative flight/minecart/boat/etc.
+            Vec3d currPos = player.getEntityPos(); // getEntityPos updates once per tick
+            if (prevPos == null) {
+                prevPos = currPos;
+                return "";
+            }
+            long currTick = world.getTime();
+            double velocity = prevPos.distanceTo(currPos);
+            if (prevTick == currTick) return String.format("%.3f", prevVel * 20);
+            prevPos = currPos;
+            prevVel = velocity;
+            prevTick = currTick;
+            return String.format("%.3f", velocity * 20);
         });
     }
 }
