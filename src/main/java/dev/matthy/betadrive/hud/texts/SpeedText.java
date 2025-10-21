0727e01ab1740ed97d6ab03a181d0b6135fa215c
karpejev <glebkarpejev828@gmail.com>
2025-10-21 15:02:40 +0300

    fix for when tickrate > framerate

--- src/main/java/dev/matthy/betadrive/hud/texts/SpeedText.java
+++ src/main/java/dev/matthy/betadrive/hud/texts/SpeedText.java
@@ -5,17 +5,18 @@ import net.minecraft.util.math.Vec3d;
 
 public class SpeedText extends HUDText {
     private static Vec3d prevPos = null;
-    private static long prevTick = 0;
+    private static int prevTick = 0;
     private static double prevVel = 0;
     public SpeedText() {
         super("MPS", (player, world) -> { // this function rocks! really not bad! Does account for elytra/creative flight/minecart/boat/etc.
             Vec3d currPos = player.getEntityPos(); // getEntityPos updates once per tick
+            int currTick = player.age;
             if (prevPos == null) {
                 prevPos = currPos;
+                prevTick = currTick;
                 return "";
             }
-            long currTick = world.getTime();
-            double velocity = prevPos.distanceTo(currPos);
+            double velocity = prevPos.distanceTo(currPos) / (currTick - prevTick);
             if (prevTick == currTick) return String.format("%.3f", prevVel * 20);
             prevPos = currPos;
             prevVel = velocity;
